35402f60c95ad938066acfd779f5ffcb05a4494a
michaelaboah <maboah@PlexusParc.com>
2022-04-17 15:40:22 -0400

    Working: Added Json Persistance

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 /*** @author Anita Louise Farah ***/
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
 import Controller.loginController;
+import Model.Card;
+import Model.SavedData;
+import Model.User;
+import utility.JsonUtil;
+import utility.MainData;
 
 
 public class Main {
@@ -8,7 +20,9 @@ public class Main {
     loginController loginCntl;
         
     public static void main(String[] args) {
-        
+       
+        SavedData.readAll("SavedAll.json");
+
         loginController loginCntl = new loginController();
             
     }
